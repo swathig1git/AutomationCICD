7908c4eb9b54964575423985445530eea380a108
Swathi Gadde <swathi.g1@gmail.com>
2025-09-17 16:25:52 -0400

    maven build changes

--- src/test/java/com/RetryAnalyzer/test/ParameterizedTest.java
+++ src/test/java/com/RetryAnalyzer/test/ParameterizedTest.java
@@ -1,12 +1,15 @@
 package com.RetryAnalyzer.test;
 
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class ParameterizedTest {
     @Test
     @Parameters("browser")
+//    public void testInBrowser(@Optional("chrome") String browser){
     public void testInBrowser(String browser){
+
         System.out.println("Running in " + browser);
     }
 }
